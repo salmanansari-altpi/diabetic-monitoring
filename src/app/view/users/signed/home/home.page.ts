60a8ee12f64fecfac397c920f9cb9baf09743299
shashoto <shashotolaha7709@gmail.com>
2023-12-12 14:43:33 +0530

    appled the snooze part

--- src/app/view/users/signed/home/home.page.ts
+++ src/app/view/users/signed/home/home.page.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Haptics } from '@capacitor/haptics';
-import { LocalNotifications } from '@capacitor/local-notifications';
+import {
+  LocalNotifications,
+  ScheduleOptions,
+} from '@capacitor/local-notifications';
 import { AlertController } from '@ionic/angular';
 import { DatabaseService } from 'src/shared/database/database.service';
 import { NotificationService } from 'src/shared/notification/notification.service';
 import { App } from '@capacitor/app';
+import { TimeoutConfig } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -24,6 +28,10 @@ export class HomePage implements OnInit {
 
   deletelistener: any;
 
+  snoozeCount: number;
+
+  setTimeOutId: number | undefined;
+
   constructor(
     private alertController: AlertController,
     private db: DatabaseService,
@@ -33,15 +41,26 @@ export class HomePage implements OnInit {
   }
 
   async ngOnInit() {
+    // let id = setTimeout(() => {
+    //   console.log("hii")
+    // },200)
+    // console.info("set time out id : ",id)
+
     // CLICKED
     await LocalNotifications.addListener(
       'localNotificationActionPerformed',
       async (noti: any) => {
         console.log(noti);
         this.eventName = noti.notification.title;
-        
+
         console.log('the noti is click', this.deletelistener);
-        await this.sugarAlertHandler();
+        if (this.setTimeOutId != undefined) {
+          clearTimeout(this.setTimeOutId);
+          this.setTimeOutId = undefined;
+        }
+        await this.sugarAlertHandler().then(async () => {
+          await this.removeListener();
+        });
       }
     );
 
@@ -49,27 +68,42 @@ export class HomePage implements OnInit {
     await LocalNotifications.addListener(
       'localNotificationReceived',
       async (notification) => {
-
-        // Haptics.vibrate({ duration: 1000 });
         this.deletelistener = true;
+        let data: object;
+
         console.log('the noti is recevie ', this.deletelistener);
 
-        const data = {
-          eventName: notification.title,
-          date: new Date().toString(),
+        data = {
+          flag: false,
         };
 
         await this.db.addTransaction(data).then(() => {
+          this.setTimeOutId = Number(
             setTimeout(async () => {
               if (this.deletelistener === true) {
-                console.log('after 2.5 seconds');
-                await this.removeListener();
+                console.log('after 20000 seconds');
+                this.snoozeCount = this.snoozeCount + 1;
+                await this.removeListener().then(
+                  async () => await this.snoozeNoti(this.snoozeCount)
+                );
+
+                if (notification.id === 103) {
+                  data = {
+                    eventName: this.eventName,
+                    sugarLevel: this.sugarLevel,
+                    dose: this.insulin,
+                    date: new Date().toString(),
+                    action: 'Reject',
+                    flag: true,
+                  };
+                  await this.db.addTransaction(data);
+                }
               }
-            }, 10000);
-          });
+            }, 20000)
+          );
+        });
       }
-    )
-    // 
+    );
 
     // if(this.deletelistener === true){
     //   setTimeout(async () => {
@@ -81,6 +115,65 @@ export class HomePage implements OnInit {
     //   }, 2500);
     // }
     this.getPendingNotifications();
+  }
+
+  async snoozeNoti(data: number) {
+    if (data === 1) {
+      const options: ScheduleOptions = {
+        notifications: [
+          {
+            id: 101,
+            title: this.eventName,
+            body: this.eventName,
+            schedule: {
+              // at: new Date(new Date().getTime() + 50000),
+              at: new Date(new Date().getTime() + 2500),
+              count: 1,
+              allowWhileIdle: true,
+            },
+            channelId: 'diabetic',
+          },
+        ],
+      };
+      await LocalNotifications.schedule(options);
+    } else if (data === 2) {
+      const options: ScheduleOptions = {
+        notifications: [
+          {
+            id: 102,
+            title: this.eventName,
+            body: this.eventName,
+            schedule: {
+              // at: new Date(new Date().getTime() + 100000),
+              at: new Date(new Date().getTime() + 5000),
+              count: 1,
+              allowWhileIdle: true,
+            },
+            channelId: 'diabetic',
+          },
+        ],
+      };
+      await LocalNotifications.schedule(options);
+    } else {
+      const options: ScheduleOptions = {
+        notifications: [
+          {
+            id: 103,
+            title: this.eventName,
+            body: this.eventName,
+            schedule: {
+              // at: new Date(new Date().getTime() + 1500000),
+              at: new Date(new Date().getTime() + 10000),
+              count: 1,
+              allowWhileIdle: true,
+            },
+            channelId: 'diabetic',
+          },
+        ],
+      };
+      await LocalNotifications.schedule(options);
+    }
+    this.snoozeCount = 0;
   }
 
   async removeListener() {
@@ -162,20 +255,22 @@ export class HomePage implements OnInit {
     console.log(data);
 
     if (role === 'ok') {
-      this.addTransaction();
+      let data = {
+        eventName: this.eventName,
+        sugarLevel: this.sugarLevel,
+        dose: this.insulin,
+        date: new Date().toString(),
+        action: 'Success',
+        flag: true,
+      };
+      this.addTransaction(data);
     }
+    return;
   }
 
-  async addTransaction() {
-    const data = {
-      eventName: this.eventName,
-      sugarLevel: this.sugarLevel,
-      dose: this.insulin,
-      date: new Date().toString(),
-      action: 'Success',
-    };
+  async addTransaction(data: any) {
     console.log('data for update Transaction ', data);
-    await this.db.updateTransaction(data);
+    await this.db.addTransaction(data);
     this.sugarLevel = 0;
     this.insulin = '';
   }
